f5c72be1075c6e935252ac80e524bb799d2911f8
Sandeep Amarnath <samarnath@oanda.com>
2023-11-07 16:10:04 -0500

    typos fixed

--- src/app/components/QuizApp.tsx
+++ src/app/components/QuizApp.tsx
@@ -24,7 +24,7 @@ function App() {
         <h1>Charged Up</h1>
         <h3>Discover Your Ideal Financial Path with OANDA</h3>
         {/* <p>
-          Hello, my name is George, it's great to meet you! I am robot designed
+          Hello, my name is George, it&apos;s great to meet you! I am robot designed
           to help humans make decisions.
         </p>
         <p>
@@ -32,18 +32,18 @@ function App() {
           our products are the best fit for your needs.
         </p> */}
         <p>
-          Hello, my name is George, it's great to meet you! I am a robot
+          Hello, my name is George, it&apos;s great to meet you! I am a robot
           designed to help humans make decisions. I was built by OANDA to assist
           people just like you in determining which of our products are the best
           fit for your needs.
           <br></br>
           <br></br>
           Discover the Perfect Financial Tools with OANDA! Are you ready to
-          embark on a journey towards financial success? Whether you're new to
-          investing or an experienced trader, our personalized recommendations
-          will guide you to the tools and knowledge you need for your financial
-          goals. Click the 'Start' button below, and let's get started on your
-          financial adventure!
+          embark on a journey towards financial success? Whether you&apos;re new
+          to investing or an experienced trader, our personalized
+          recommendations will guide you to the tools and knowledge you need for
+          your financial goals. Click the &apos;Start&apos; button below, and
+          let&apos;s get started on your financial adventure!
         </p>
         <Image
           className="rounded-lg ml-[-2rem] "
